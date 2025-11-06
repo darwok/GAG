c2f7e62f633302904f3b00783e29849b6ecf7bb0
darwok <darwok.bit@outlook.com>
2025-11-06 01:06:15 -0600

    Part 1 of 2
    Added fade (half), added jump counter, fixed DOTween errors, created forlders to import scripts and assets.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,93 @@
+using DG.Tweening.Core.Easing;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movimiento")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+
+    [Header("Comprobación de suelo")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float moveInput;
+    private Animator anim;
+
+    public bool blockInput;
+    private int jumpCounter;
+    private bool facingRight = false;
+
+    private const int MaxJumps = 2;
+
+    void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        
+        if (blockInput) return;
+
+        // Entrada horizontal
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        // Saltar solo si está en el suelo
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            jumpCounter++;
+
+            if (jumpCounter >= MaxJumps) return;
+
+            jumpCounter = Mathf.Clamp(jumpCounter, 0, MaxJumps - 1);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            anim.SetTrigger("Jump");
+        }
+
+        if (!facingRight && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+    public void BlockInput(bool value)
+    {
+        blockInput = value;
+    }
+
+    void FixedUpdate()
+    {
+        // Movimiento lateral
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+
+        // Verificar si está tocando el suelo
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }
