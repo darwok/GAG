452c1fc5b8829a4a7ac5bfe4ce7ff04305aa54dc
darwok <darwok.bit@outlook.com>
2025-11-20 07:08:37 -0600

    Goal Fix
    Fixed an error with Goal script. it now works fine.

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (!other.CompareTag("Player")) return;
+        EventBus.OnLevelWon?.Invoke();
     }
 }
